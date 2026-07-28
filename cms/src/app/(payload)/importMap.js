09ed3ad0d81dc8bb1ccdc6aceaf744d5de3b720b
amrshah <amr.shah@gmail.com>
2026-07-28 20:02:05 +0500

    fix(cms): add CollectionCards RSC mapping in importMap.js

--- cms/src/app/(payload)/importMap.js
+++ cms/src/app/(payload)/importMap.js
@@ -1,1 +1,5 @@
-export const importMap = {};
+import { CollectionCards as CollectionCards_0 } from '@payloadcms/next/rsc';
+
+export const importMap = {
+  '@payloadcms/next/rsc#CollectionCards': CollectionCards_0,
+};
