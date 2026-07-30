27dad4fa1f44cd66ad2b47965d76f8e9a496b395
amrshah <amr.shah@gmail.com>
2026-07-30 17:13:16 +0500

    feat: implement robust postgres migration workflow

---
+++ cms/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260730_121253 from './20260730_121253';
+
+export const migrations = [
+  {
+    up: migration_20260730_121253.up,
+    down: migration_20260730_121253.down,
+    name: '20260730_121253'
+  },
+];
